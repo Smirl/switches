e7ccac3967dc1074ace48d359a52cb9113ad25f8
Alex Williams <alex.williams@skyscanner.net>
2021-05-18 16:34:16 +0100

    Change the postgresql prefix

--- switches.py
+++ switches.py
@@ -18,10 +18,15 @@ from flask_sqlalchemy import SQLAlchemy
 # fun though.
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'ghjkl;IU3jnca;hFIAZ'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['TESTING'] = os.environ.get('TESTING', False) == 'True'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///switches.sqlite')
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "ghjkl;IU3jnca;hFIAZ"
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+app.config["TESTING"] = os.environ.get("TESTING", False) == "True"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///switches.sqlite"
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
+    "postgres://", "postgresql://"
+)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
@@ -35,7 +40,7 @@ class Switch(db.Model):
 
     """The on off value in the database."""
 
-    __tablename__ = 'switches'
+    __tablename__ = "switches"
 
     id_ = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(64), nullable=False, unique=True)
@@ -46,11 +51,11 @@ class Switch(db.Model):
     def to_dict(self):
         """A dict representation for using with json."""
         return {
-            'id_': self.id_,
-            'slug': self.slug,
-            'created': self.created,
-            'updated': self.updated,
-            'value': self.value,
+            "id_": self.id_,
+            "slug": self.slug,
+            "created": self.created,
+            "updated": self.updated,
+            "value": self.value,
         }
 
     def flip(self):
@@ -68,17 +73,17 @@ class Switch(db.Model):
 
 
 # This is the event listener
-db.event.listen(Switch.value, 'set', Switch.on_changed_value)
+db.event.listen(Switch.value, "set", Switch.on_changed_value)
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
     """Render the homepage template. Also pass in all of the switches."""
     switches = Switch.query.all()
-    return render_template('index.html', switches=switches)
+    return render_template("index.html", switches=switches)
 
 
-@app.route('/api/flip/<id_>', methods=['POST'])
+@app.route("/api/flip/<id_>", methods=["POST"])
 def api_flip(id_):
     """Flip the switch with the given id. Return new state of that switch."""
     s = Switch.query.get_or_404(id_)
@@ -86,31 +91,31 @@ def api_flip(id_):
     return jsonify(s.to_dict())
 
 
-@app.route('/api/add', methods=['POST'])
+@app.route("/api/add", methods=["POST"])
 def api_add():
     """
     Add a new Switch using the slug given in the form.
 
     Note this isn't really an API method, it returns a redirect to the homepage.
     """
-    s = Switch(slug=request.form['slug'].strip())
+    s = Switch(slug=request.form["slug"].strip())
     db.session.add(s)
     db.session.commit()
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/api/delete/<id_>', methods=['POST'])
+@app.route("/api/delete/<id_>", methods=["POST"])
 def api_delete(id_):
     """Delete the Switch with the id. Delete something twice is ok."""
     s = Switch.query.get(id_)
     if s is not None:
         db.session.delete(s)
         db.session.commit()
-        message = 'Deleted {}'.format(id_)
+        message = "Deleted {}".format(id_)
     else:
-        message = 'Did not find {}'.format(id_)
-    return jsonify({'message': message})
+        message = "Did not find {}".format(id_)
+    return jsonify({"message": message})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
